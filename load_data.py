20b97b32b5bc406a96f349bd70991029d2752d06
jenny <jennydoan03@gmail.com>
2025-10-20 21:40:14 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -7,7 +7,7 @@ brands = pd.read_csv('brand_detail.csv')
 spend = pd.read_csv('daily_spend.csv')
 
 # Connect to Azure SQL
-conn_str = 'Server=tcp:day8exercise.database.windows.net,1433;Initial Catalog=Day8Exercise;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";'
+conn_str = 'Server=tcp:day8exercise.database.windows.net,1433;Initial Catalog=Day8Exercise;Persist Security Info=False;User ID=jenny;Password=day8exercise!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;'
 engine = sqlalchemy.create_engine(f'mssql+pyodbc:///?odbc_connect={conn_str}')
 
 # Load tables
